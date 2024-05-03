bd93cffa7c9f5d03d648a9f6ea98b654ec6f60d9
darshanasam1234 <64640091+darshanasam1234@users.noreply.github.com>
2024-05-03 22:01:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "myproddeployment12345"{
     location="eastus"
     name="myproddeployment123456"
 }
-resource "azurerm_resource_group" "myproddeployment123454446"{
+resource "azurerm_resource_group" "myproddeployment12345444678"{
     location="eastus"
     name="myproddeployment123456"
 }
