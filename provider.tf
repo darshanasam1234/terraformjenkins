07ef422e69f5962998679b013295f70d1253b5e4
darshanasam1234 <64640091+darshanasam1234@users.noreply.github.com>
2024-05-04 01:44:15 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,5 +11,8 @@ provider "azurerm" {
   # Configuration options
 
   tenant_id = "32b4d603-89f1-413b-b743-fd7b6dfaa2a5"
+  client_secret = "UEG8Q~BA6C1pw-2awL4Kiu6BJ~_0LCAsh_BVIbFz"
+  subscription_id = "d1a072bc-69ca-42c9-b02c-648ba7862db2"
+  client_id = "c9f84670-ec38-4895-91d9-be816b1f842b"
   features {}
 }
